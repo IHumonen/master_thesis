e7a9b60745a354063584800922b826905789f9d5
kesha_humonen <g1410ish@gmail.com>
2022-03-31 02:01:37 +0300

    add corpus classes and batching

---
+++ utils/corpus_utils.py
@@ -0,0 +1,112 @@
+import pymorphy2
+import torch
+
+from razdel import tokenize
+from torch_geometric.data import Data
+
+class Morpher:
+    """pymorphy with cash"""
+    def __init__(self, pymorphy_morpher):
+        self.morpher = pymorphy_morpher
+        self.cash = dict()
+        
+    def __call__(self, word):        
+        if word in self.cash:
+            return self.cash[word]
+        else:
+            full_info = self.morpher.parse(word)[0]
+            lemma = full_info.normal_form
+            pos = full_info.tag.POS
+            self.cash[word] = (lemma, pos)
+            return (lemma, pos)
+
+class Corpus():
+    """a container for edges and token-index dictionaries"""
+    def __init__(self, graph, token2idx, idx2token):
+        self.graph = graph
+        self.token2idx = token2idx
+        self.idx2token = idx2token
+
+class CorpusMaker():
+    """creates a Corpus objects from a collection of texts"""
+    def __init__(self, tokenizer='razdel', morpher='pymorphy2', window_size=3):
+        self.tokenizer = tokenizer
+        if self.tokenizer == 'razdel':
+            self.tokenizer = razdel_tokenize
+
+        self.morpher = morpher
+        if self.morpher is not None:
+            self.morpher = Morpher(pymorphy2.MorphAnalyzer())
+
+        self.window_size = window_size
+       
+    def texts2tokens(self, raw_texts, create_dicts=True):
+        if create_dicts:
+            unique_tokens = set()
+        texts = []
+        for text in raw_texts:
+            if self.tokenizer is not None:
+                tokenized = self.tokenizer(text)
+            else:
+                tokenized = raw_texts
+            if self.morpher is not None:
+                lemmas = []
+                for token in tokenized:
+                    lemma, pos = self.morpher(token)
+                    if pos in ['NOUN', 'INFN', 'VERB', 'ADJF']:
+                        lemmas.append(lemma)
+                        if create_dicts:
+                            unique_tokens.add(token)
+                texts.append(lemmas)
+            else:
+                texts.append(tokenize)
+
+        if create_dicts:    
+            token2idx = dict(zip(unique_tokens, range(len(unique_tokens))))
+            idx2token = dict(zip(range(len(unique_tokens)), unique_tokens))
+
+            return texts, token2idx, idx2token
+        else:
+            return texts
+
+    def tokens2graph(self, texts, token2idx, idx2token):
+        edges = []
+        for text in texts:
+            for i, token in enumerate(text):
+                if token in token2idx:
+                    left = max(i-self.window_size, len(text))
+                    right = min(i+self.window_size, len(text))
+                    neighbours = text[left: i] + text[i+1: right]
+                    for neighbour in neighbours:
+                        if neighbour in token2idx:
+                            edge = [token2idx[token], token2idx[neighbour]]
+                            if edge != [] and edge[0] != edge[1] and not (edge in edges):
+                                edges.append(edge)
+
+        x = torch.tensor(list(idx2token.keys()))
+        if edges == []:
+            edges = [[0,1]]
+            print('empty ===========================')
+        edge_index = torch.tensor(edges).transpose(0, 1)
+        graph = Data(x = x.long(), edge_index = edge_index)
+
+        return Corpus(graph, token2idx, idx2token) 
+
+    def create_graph(self, raw_texts, dicts=None):
+        if dicts is not None:
+            token2idx, idx2token = dicts[0], dicts[1]
+            tokens = self.texts2tokens(raw_texts, create_dicts=False)
+        else:
+            tokens, token2idx, idx2token = self.texts2tokens(raw_texts, create_dicts=True)
+
+        return self.tokens2graph(tokens, token2idx, idx2token)
+    
+
+def razdel_tokenize(text):
+    tokens = []
+    tokens_raw = list(tokenize(text))
+    for token in tokens_raw:
+        tokens.append(token.text)
+
+    return tokens
+
---
+++ utils/model_utils.py
@@ -0,0 +1,115 @@
+import os
+
+import torch
+import wandb
+
+import pandas as pd
+
+def train(model, 
+            data,
+            idx2token, 
+            optimizer, 
+            criterion,
+            epoch,
+            device):
+
+    print(f"epoch {epoch}")
+
+    epoch_loss = 0
+    model.train()
+    
+    optimizer.zero_grad()
+
+    predictions, ys = model(data.to(device)).to(device), torch.tensor(list(idx2token.keys())).to(device)
+
+    loss = criterion(predictions.view(-1, predictions.size(-1)), ys.view(-1))
+    loss.backward()
+
+    optimizer.step()
+
+    batch_loss = loss.item()
+    epoch_loss = batch_loss
+    
+    return epoch_loss
+
+def evaluate(model, 
+                data,
+                idx2token, 
+                criterion, 
+                epoch,
+                device,
+                timestamp,
+                save_checkpoints=False
+                ):
+    epoch_loss = 0
+
+    #    model.train(False)
+    model.eval()
+
+    with torch.no_grad():
+
+        predictions, ys = model(data.to(device)).to(device), torch.tensor(list(idx2token.keys())).to(device)
+
+        loss = criterion(predictions.view(-1, predictions.size(-1)), ys.view(-1))
+
+        epoch_loss += loss.item()
+
+    if save_checkpoints:
+        file_name = 'epoch_' + str(epoch) + '_' + timestamp + '.pt'
+        path = os.path.expanduser("".join(['logs/', file_name]))
+        torch.save(model.state_dict(), path)
+
+        print(f"epoch loss {epoch_loss}")
+    print(
+        "========================================================================================================")
+
+    
+    return epoch_loss
+
+
+def get_item(d, key):
+    if key in d:
+        return d[key]
+    else:
+        return None 
+
+def update_log():
+    api = wandb.Api()
+
+    # Project is specified by <entity/project-name>
+    runs = api.runs("kesha_humonen/master_thesis")
+
+    summary_list, config_list, name_list, created_list = [], [], [], []
+    for run in runs: 
+        # .summary contains the output keys/values for metrics like accuracy.
+        #  We call ._json_dict to omit large files 
+        summary_list.append(run.summary._json_dict)
+
+        # .config contains the hyperparameters.
+        #  We remove special values that start with _.
+        config_list.append(
+            {k: v for k,v in run.config.items()
+              if not k.startswith('_')})
+
+        # .name is the human-readable name of the run.
+        name_list.append(run.name)
+        
+        created_list.append(run._attrs['createdAt'])
+
+    runs_df = pd.DataFrame({
+        "summary" : summary_list,
+        "config" : config_list,
+        "name" : name_list,
+        'created' : created_list
+        })
+
+
+    for key in summary_list[0]:
+        runs_df[key] = runs_df['summary'].apply(get_item, args=(key,))
+    for key in config_list[0]:
+        runs_df[key] = runs_df['config'].apply(get_item, args=(key,))
+
+
+    runs_df.to_csv("wandb_log.csv")
+    
+    return runs_df
